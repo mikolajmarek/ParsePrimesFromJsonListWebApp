3404ed3308d43c7d1a8f25d141ecfbb0f4147508
mikolajmarek <mikolaj.juras@gmail.com>
2018-05-15 20:17:27 +0200

    Spring boot is starting....

---
+++ src/main/java/pl/mj/webApplication/controller/HomeController.java
@@ -0,0 +1,13 @@
+package pl.mj.webApplication.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+}
